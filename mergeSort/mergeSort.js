7f127095b98da1f99a80bceebe4decd75fa888fe
souravshetty <sourav.s.shetty@gmail.com>
2025-07-06 23:45:48 -0700

    merge sort soln

---
+++ mergeSort/mergeSort.js
@@ -0,0 +1,33 @@
+let arr = [6, 5, 3, 2];
+
+function sortArray(arr) {
+	if (arr.length <= 1) return arr;
+
+	let mid = Math.floor(arr.length / 2); 
+	let left = sortArray(arr.slice(0, mid));
+	let right = sortArray(arr.slice(mid));
+
+	return merge(left, right);
+}
+
+function merge(left, right) {
+	let res = [];
+	let i = 0,
+		j = 0;
+
+	while (i < left.length && j < right.length) {
+		
+		if (left[i] < right[j]) {
+			
+			res.push(left[i]);
+			i++;
+		} else {
+			res.push(right[j]);
+			j++;
+		}
+	}
+
+	return [...res, ...left.slice(i), ...right.slice(j)];
+}
+
+console.log(sortArray(arr)); 
