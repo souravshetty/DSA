9fe9b1bc39eecec97face55e75d7d3859913e3fb
souravshetty <sourav.s.shetty@gmail.com>
2025-08-13 18:50:08 -0700

    Solution for 58 on LC

---
+++ Strings/lengthwithout any Method.js
@@ -0,0 +1,21 @@
+var lengthOfLastWord = function (s) {
+	let n = s.length - 1;
+	while (n >= 0) {
+		if (s[n] === " ") {
+			n--;
+		} else {
+			break;
+		}
+	}
+	let count = 0;
+	while (n >= 0) {
+		if (s[n] != " ") {
+			n--;
+			count++;
+		} else {
+			break;
+		}
+	}
+
+	return count;
+};
