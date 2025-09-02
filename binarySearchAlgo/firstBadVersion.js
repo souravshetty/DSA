292c70e84cb0b0e292191f24f8af8e91fde4260f
souravshetty <sourav.s.shetty@gmail.com>
2025-09-02 01:52:29 -0700

    solution for LC 278

---
+++ binarySearchAlgo/firstBadVersion.js
@@ -0,0 +1,20 @@
+var solution = function (isBadVersion) {
+	/**
+	 * @param {integer} n Total versions
+	 * @return {integer} The first bad version
+	 */
+	return function (n) {
+		let l = 1;
+		let r = n;
+		while (l < r) {
+			let m = l + Math.floor((r - l) / 2);
+			if (!isBadVersion(m)) {
+				l = m + 1;
+			} else {
+				r = m;
+			}
+		}
+
+		return r;
+	};
+};
