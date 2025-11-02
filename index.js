190c89ced65788bc7ba563f2e46be1f56357b94d
souravshetty <sourav.s.shetty@gmail.com>
2025-11-02 09:33:49 -0800

    single number LC

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 
-let container =document.getElementById('pattern');
-for (let i = 0; i < 4; i++) {
-	let row = "";
-	for (let j = 0; j < i + 1; j++) {
-		row = row + "*";
-	}
-	const line=document.createElement("div");
-    line.textContent=row;
-    container.appendChild(line);
+// let container =document.getElementById('pattern');
+// for (let i = 0; i < 4; i++) {
+// 	let row = "";
+// 	for (let j = 0; j < i + 1; j++) {
+// 		row = row + "*";
+// 	}
+// 	const line=document.createElement("div");
+//     line.textContent=row;
+//     container.appendChild(line);
+// }
+
+let arr=[4,1,1,4,2]
+let xor=0;
+for(let i=0;i<arr.length;i++){
+	xor=xor^arr[i];
 }
+console.log(xor) ;
